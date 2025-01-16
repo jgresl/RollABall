2716c490b03de900b607aa67768e836b74652226
Jonathan Gresl <jgresl@hotmail.com>
2025-01-16 02:53:40 -0700

    Adjust ball controller script to log when w is pressed

--- Assets/_Scripts/BallController.cs
+++ Assets/_Scripts/BallController.cs
@@ -5,12 +5,15 @@ public class BallController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Calling the Start method");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Calling the Update method");
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("User's Input: W");
+        }
     }
 }
